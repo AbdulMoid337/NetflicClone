0b6aabfc91ec54b220822b18670a7ba8601aa8af
Abdul Moid <abdulmoid337@gmail.com>
2024-08-03 20:23:14 +0530

    Netflix Clone

---
+++ img.js
@@ -0,0 +1,6 @@
+// let imgg = document.getElementsByClassName("oldimg")
+// for (let i = 0; i < imgg.length; i++) {
+//     imgg[i].src = "https://www.reuters.com/resizer/ctrKM6DTFHeq8QXTGxNCbofmEig=/960x0/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/YV24L2VRWBJUHH3DXOOQWVJNGM.jpg"
+//     console.log(`value of image no ${1} is changed`,imgg[i].src)
+// }
+console.dir(document.querySelectorAll("secImg img"))
